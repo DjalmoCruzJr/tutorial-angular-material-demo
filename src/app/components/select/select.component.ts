50496a47d326e0a26c3bd4ecab50d8725496c774
Djalmo Cruz Jr <djalmo.cruz@gmail.com>
2019-08-22 16:30:19 -0300

    adding SELECT example code & fixing RADIO css example code

--- src/app/components/select/select.component.ts
+++ src/app/components/select/select.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+export interface Food {
+  value: string;
+  viewValue: string;
+}
 
 @Component({
   selector: 'app-select',
   templateUrl: './select.component.html',
   styleUrls: ['./select.component.css']
 })
-export class SelectComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class SelectComponent {
+  foods: Food[] = [
+    { value: 'steak-0', viewValue: 'Steak' },
+    { value: 'pizza-1', viewValue: 'Pizza' },
+    { value: 'tacos-2', viewValue: 'Tacos' }
+  ];
 }
